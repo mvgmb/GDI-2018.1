728f85e13bfcb7fce5bc7d285d58792b4a818eb9
mvgmb <mvgmb@cin.ufpe.br>
2018-05-22 14:11:08 -0300

    Criando conexão dos fxml's com os controladores

--- src/control/SearchLayoutController.java
+++ src/control/SearchLayoutController.java
@@ -1,11 +1,51 @@
 package control;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 import javax.swing.table.TableColumn;
 
 public class SearchLayoutController {
     @FXML
-    TableColumn CpfNameTab;
-
+    Label nomeLabel;
+    @FXML
+    Label cpfLabel;
+    @FXML
+    Label telefoneLabel;
+    @FXML
+    Label salarioLabel;
+    @FXML
+    Label dataNascimentoLabel;
+    @FXML
+    Label cpfSupervisionadorLabel;
+    @FXML
+    Label cepLabel;
+    @FXML
+    Label ruaLabel;
+    @FXML
+    Label numeroLabel;
+    @FXML
+    Label complementoLabel;
+    @FXML
+    Label bairroLabel;
+    @FXML
+    Label estadoLabel;
+    @FXML
+    Label paisLabel;
+    @FXML
+    Button pesquisarBtn;
+    @FXML
+    Button adicionarBtn;
+    @FXML
+    Button modificarBtn;
+    @FXML
+    Button removerBtn;
+    @FXML
+    void adicionarFuncionario(){}
+    @FXML
+    void modificarFuncionario(){}
+    @FXML
+    void removerFuncionario(){}
 }
